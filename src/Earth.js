aa634b46275b668de7f9bbd610a8c73dd4ca9fb1
mmargegaj <mario.margegaj@gmail.com>
2023-10-26 23:01:37 +0200

    small changes

--- src/Earth.js
+++ src/Earth.js
@@ -14,6 +14,38 @@ const Earth = () => {
 
     const myDomElement = document.querySelector(".left-side");
 
+    setTimeout(() => {
+      const div1 = document.querySelector(".scene-container");
+      if (div1) {
+        // Check if the element has a style property
+        if (div1.style) {
+          // Remove the 'position' property from the inline styles
+          div1.style.removeProperty("position");
+          console.log("sdfa");
+        }
+      }
+      // Define the XPath expression
+      const xpathExpression = "//div[@class='left-side']/div";
+
+      // Use document.evaluate to find the element
+      const result = document.evaluate(
+        xpathExpression,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      );
+
+      // Check if a matching element was found
+      if (result.singleNodeValue) {
+        // Access the element and remove the 'position' property from its inline styles
+        const element = result.singleNodeValue;
+        if (element.style) {
+          element.style.removeProperty("position");
+        }
+      }
+    }, 5000);
+
     // Event listeners
     socket.on("predictionInfo", (newPrediction) => {
       predictions.push({
