72570c8d4cbc84b713cc51a6cf9a08c791818c60
mmargegaj <mario.margegaj@gmail.com>
2023-11-01 10:33:43 +0100

    Revert "Updates (#1)" (#2)
    This reverts commit 81a3909b237eda82dca93ef67d27fe5931d5d04d.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-import { useEffect, useState } from "react";
-import io from "socket.io-client";
 import "./App.css";
 import Earth from "./Earth";
-
-const App = () => {
-  const socket = io("http://localhost:3002");
-  const [predictions, setPredictions] = useState([]);
-  const [textInfo, setTextInfo] = useState([]);
-
-  useEffect(() => {
-    // Event listeners
-    socket.on("predictionInfo", (newPrediction) => {
-      setPredictions((prevState) => [
-        ...prevState,
-        {
-          ...newPrediction.prediction,
-          maxR: 4,
-          propagationSpeed: 1,
-          repeatPeriod: 3000,
-        },
-      ]);
-
-      setTextInfo((prevState) => [...prevState, newPrediction.text]);
-    });
-
-    socket.on("eqInfo", (earthquakeInfo) => {
-      setTextInfo((prevState) => [...prevState, earthquakeInfo.text]);
-    });
-  }, [predictions, socket]);
-
+function App() {
   return (
     // <div className="App">
     <div className="container">
       <div className="left-side">
-        <Earth predictions={predictions} />
+        <Earth />
       </div>
-      <div className="right-side">
-        {textInfo.length ? (
-          <div className="text-info-container">
-            {textInfo.map((text, index) => (
-              <div key={index} className="text-box">
-                {text}
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="waiting-message">
-            No info available yet... Waiting...
-          </div>
-        )}
-      </div>
+      <div className="right-side"></div>
     </div>
     // </div>
   );
-};
+}
 
 export default App;
